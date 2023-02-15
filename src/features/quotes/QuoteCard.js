2789fc732a2db28fc13a19a445c8dee3c8ea7d4f
Parker Parham <parkerparham@gmail.com>
2023-02-15 18:28:34 -0500

    commit tree ea98b72deb1cd0d13bcfbc4499f0d1d07ba51f2b

--- src/features/quotes/QuoteCard.js
+++ src/features/quotes/QuoteCard.js
@@ -1,15 +1,32 @@
 import React from "react";
+import { removeQuote, upvoteQuote, downvoteQuote } from "./quotesSlice";
+import { useDispatch } from "react-redux";
 
-function QuoteCard(props) {
+function QuoteCard({ quote }) {
+
+
+  const dispatch = useDispatch()
+
+  function handleRemoveClick(){
+    dispatch(removeQuote(quote.id))
+  }
+
+  function handleUpvoteClick(){
+    dispatch(upvoteQuote(quote.id))
+  }
+  function handleDownvoteClick(){
+    dispatch(downvoteQuote(quote.id))
+  }
+
   return (
     <div>
       <div className="card card-inverse card-success card-primary mb-3 text-center">
         <div className="card-block">
           <blockquote className="card-blockquote">
-            <p>{/*Render Quote Content*/}</p>
+            <p>{quote.content}</p>
             <footer>
               - author{" "}
-              <cite title="Source Title">{/*Render Quote Author*/}</cite>
+              <cite title="Source Title">{quote.author}</cite>
             </footer>
           </blockquote>
         </div>
@@ -19,17 +36,17 @@ function QuoteCard(props) {
             role="group"
             aria-label="Basic example"
           >
-            <button type="button" className="btn btn-primary">
+            <button onClick={handleUpvoteClick}type="button" className="btn btn-primary">
               Upvote
             </button>
-            <button type="button" className="btn btn-secondary">
+            <button onClick={handleDownvoteClick} type="button" className="btn btn-secondary">
               Downvote
             </button>
-            <button type="button" className="btn btn-danger">
+            <button onClick={handleRemoveClick} type="button" className="btn btn-danger">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div>Votes: {/*Render Quote Votes*/}</div>
+          <div>Votes: {quote.votes}</div>
         </div>
       </div>
     </div>
